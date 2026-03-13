41b4a4d078a86b1d83888540f109b9d059afbef8
EskoRautiainen <Rautiainen98@gmail.com>
2026-03-13 21:16:31 +0200

    feat(selected_features): include Yhdistetty_kiiht_rms

--- MLmodel/src/feature_engineering.py
+++ MLmodel/src/feature_engineering.py
@@ -8,6 +8,8 @@ def step_04_engineer_features(dataframe: pd.DataFrame) -> pd.DataFrame:
         "Pysty_kiiht",
         "Sivuheilahdus_kiiht",
         "Nyökkimis_kiiht",
+        "Yhdistetty_kiiht_rms"
+
     ]
     engineered_dataframe = dataframe.loc[:, selected_features].copy()
 
@@ -16,5 +18,12 @@ def step_04_engineer_features(dataframe: pd.DataFrame) -> pd.DataFrame:
     print("The data has been prepared for feature engineering.")
     print()
     print(engineered_dataframe.head(5).to_string())
+
+    # Save results to Excel
+    output_path = "MLmodel/output/engineered_features.xlsx"
+    engineered_dataframe.to_excel(output_path, index=False)
+
+    print()
+    print(f"Feature engineered data saved to: {output_path}")
 
     return engineered_dataframe
